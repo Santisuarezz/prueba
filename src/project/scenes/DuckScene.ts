793a70ff339df5bce71d54dc1ef2e67243239d53
Santisuarezz <156725477+Santisuarezz@users.noreply.github.com>
2024-02-23 17:30:59 -0300

    agregue lineas y frase

--- src/project/scenes/DuckScene.ts
+++ src/project/scenes/DuckScene.ts
@@ -1,4 +1,4 @@
-import { Graphics, Sprite } from "pixi.js";
+import { Graphics, Sprite, Text } from "pixi.js";
 import { PixiScene } from "../../engine/scenemanager/scenes/PixiScene";
 import { ScaleHelper } from "../../engine/utils/ScaleHelper";
 
@@ -13,9 +13,25 @@ export class DuckScene extends PixiScene {
 		this.addChild(this.kratos);
 		this.cuadrado = Sprite.from("cuadrado");
 		this.addChild(this.cuadrado);
-		// this.cuadrado.position.set(0, 400);
+		this.cuadrado.position.set(0, 400);
 		this.cuadrado.width = 400;
 		this.cuadrado.scale.y = this.cuadrado.scale.x;
+
+		const myGraph: Graphics = new Graphics();
+		myGraph.lineStyle({ color: 0xff00ff, width: 5, alpha: 1 });
+		myGraph.moveTo(0, 0);
+		myGraph.lineTo(0, 0);
+		myGraph.lineTo(100, 300);
+		myGraph.lineTo(100, 100);
+
+		myGraph.position.set(1024 / 2, 768 / 2);
+		this.addChild(myGraph);
+
+		const myText: Text = new Text("asdasdasd", { fontSize: 108, fill: 0xff0000 });
+		myText.position.x = 0;
+		myText.position.y = 300;
+		myText.angle = 45;
+		myText.scale.set(3);
 	}
 
 	public override onResize(_newW: number, _newH: number): void {
@@ -26,9 +42,3 @@ export class DuckScene extends PixiScene {
 		dt;
 	}
 }
-const myGraph: Graphics = new Graphics();
-myGraph.lineStyle({ color: 0xff00ff, width: 10, alpha: 1 });
-myGraph.moveTo(0, 0);
-myGraph.lineTo(300, 500);
-
-myGraph.position.set(1024 / 2, 768 / 2);
