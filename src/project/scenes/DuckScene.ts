bc01adc187adbc0b0a91495ecc2bf409d1d4eb28
Santisuarezz <156725477+Santisuarezz@users.noreply.github.com>
2024-02-26 16:17:00 -0300

    cuadros

--- src/project/scenes/DuckScene.ts
+++ src/project/scenes/DuckScene.ts
@@ -1,37 +1,60 @@
-import { Graphics, Sprite, Text } from "pixi.js";
+import { Sprite } from "pixi.js";
 import { PixiScene } from "../../engine/scenemanager/scenes/PixiScene";
 import { ScaleHelper } from "../../engine/utils/ScaleHelper";
+import { SantiButton } from "./SantiButton";
+import { Manager } from "../..";
 
 export class DuckScene extends PixiScene {
 	public static readonly BUNDLES = ["package-2"];
 
 	private kratos;
-	private cuadrado;
+	// private cuadrado;
+
 	constructor() {
 		super();
 		this.kratos = Sprite.from("kratos");
 		this.addChild(this.kratos);
-		this.cuadrado = Sprite.from("cuadrado");
-		this.addChild(this.cuadrado);
-		this.cuadrado.position.set(0, 400);
-		this.cuadrado.width = 400;
-		this.cuadrado.scale.y = this.cuadrado.scale.x;
 
-		const myGraph: Graphics = new Graphics();
-		myGraph.lineStyle({ color: 0xff00ff, width: 5, alpha: 1 });
-		myGraph.moveTo(0, 0);
-		myGraph.lineTo(0, 0);
-		myGraph.lineTo(100, 300);
-		myGraph.lineTo(100, 100);
+		// this.cuadrado = Sprite.from("cuadrado");
+		// this.addChild(this.cuadrado);
+		// this.cuadrado.position.set(0, 400);
+		// this.cuadrado.width = 400;
+		// this.cuadrado.scale.y = this.cuadrado.scale.x;
 
-		myGraph.position.set(1024 / 2, 768 / 2);
-		this.addChild(myGraph);
+		// const myGraph: Graphics = new Graphics();
+		// myGraph.beginFill(0x386ddf);
+		// myGraph.drawRect(-100, -100, 200, 200);
+		// myGraph.eventMode = "static";
+		// myGraph.on("pointertap", () => {
+		// 	console.log("Tapeado");
+		// });
+		// myGraph.on("pointerover", () => {}, this);
+		// myGraph.on("pointerout", () => {
+		// 	console.log("Salio");
+		// });
 
-		const myText: Text = new Text("asdasdasd", { fontSize: 108, fill: 0xff0000 });
-		myText.position.x = 0;
-		myText.position.y = 300;
-		myText.angle = 45;
-		myText.scale.set(3);
+		const firstBtn = new SantiButton(() => {
+			console.log("Tap first");
+		}, 0x00ffff);
+		firstBtn.position.set(Manager.width / 2, Manager.height * 0.4);
+
+		const secondBtn = new SantiButton(() => {
+			console.log("Tap second");
+		}, 0xff00ff);
+		secondBtn.position.set(Manager.width / 2, Manager.height * 0.6);
+
+		this.addChild(firstBtn, secondBtn);
+
+		// myGraph.position.set(1024 / 2, 768 / 2);
+		// this.addChild(myGraph);
+
+		// const myText: Text = new Text("asdasdasd", { fontSize: 108, fill: 0xff0000 });
+		// myText.position.x = 0;
+		// myText.position.y = 300;
+		// myText.angle = 45;
+		// myText.scale.set(3);
+
+		// this.kratos.addChild(myText);
 	}
 
 	public override onResize(_newW: number, _newH: number): void {
