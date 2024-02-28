bb72dbd6acd96b515a1cff322a458bfb2567ba09
Santisuarezz <156725477+Santisuarezz@users.noreply.github.com>
2024-02-28 18:07:46 -0300

    cambio de botones

---
+++ src/project/elements/Settingsb.ts
@@ -0,0 +1,10 @@
+import { Container, Sprite } from "pixi.js";
+export class Settingsb extends Container {
+	constructor() {
+		super();
+
+		const asdasd: Sprite = Sprite.from("menu");
+
+		// this.addChild(asdasd);
+	}
+}
