53aff4bda837367ad9aa48d6194a1bef18ad3da5
Santisuarezz <156725477+Santisuarezz@users.noreply.github.com>
2024-03-05 16:08:04 -0300

    agregue teclas

--- src/project/elements/Settingsb.ts
+++ src/project/elements/Settingsb.ts
@@ -1,4 +1,6 @@
 import { Container, Sprite } from "pixi.js";
+import { Keyboard } from "../../engine/input/Keyboard";
+import { Key } from "../../engine/input/Key";
 export class Settingsb extends Container {
 	constructor() {
 		super();
@@ -6,5 +8,9 @@ export class Settingsb extends Container {
 		const asdasd: Sprite = Sprite.from("menu");
 
 		this.addChild(asdasd);
+		const keyboard = new Keyboard();
+		keyboard.pressed.on(Key.KEY_W, () => {});
+
+		console.log("apreto w");
 	}
 }
