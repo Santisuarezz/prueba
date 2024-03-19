5cedf1b1f450e86e7bb0eaf0a8c91f35df8270f8
Santisuarezz <156725477+Santisuarezz@users.noreply.github.com>
2024-03-19 14:20:13 -0300

    solucion de erorres

--- src/project/elements/Bala.ts
+++ src/project/elements/Bala.ts
@@ -1,14 +1,15 @@
-import { Container, Sprite } from "pixi.js";
+import { Sprite } from "pixi.js";
+import { Container } from "pixi.js";
 import { Tween } from "tweedle.js";
 
 export class Bala extends Container {
-	private bala: Sprite = Sprite.from(balas);
+	private bala: Sprite;
 	constructor(xTarget: number) {
 		super();
-
+		this.bala = Sprite.from("balas");
 		this.addChild(this.bala);
 
-		new Tween(balaGraph)
+		new Tween(this.bala)
 			.to({ x: xTarget }, 2000)
 			.start()
 			.onComplete(() => {
